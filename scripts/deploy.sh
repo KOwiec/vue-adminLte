43ab44d588c0ae40b41cf94b9760399dfa44d8e1
KOwiec <owczarz@protonmail.com>
2021-05-25 17:41:29 +0200

    change deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:KOwiec/vue-adminLte.git main:gh-pages
